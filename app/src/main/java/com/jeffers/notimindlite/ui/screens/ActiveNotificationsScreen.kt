ecae99bbfa0573b9c3ec08bd95d20d9d6163e7d0
Freya Jeffers <fboniface@unr.edu>
2026-08-12 22:42:37 -0700

    Restore layout wildcard import in ActiveNotificationsScreen

--- app/src/main/java/com/jeffers/notimindlite/ui/screens/ActiveNotificationsScreen.kt
+++ app/src/main/java/com/jeffers/notimindlite/ui/screens/ActiveNotificationsScreen.kt
@@ -12,8 +12,7 @@ import androidx.compose.animation.expandVertically
 import androidx.compose.animation.fadeIn
 import androidx.compose.animation.fadeOut
 import androidx.compose.animation.shrinkVertically
-import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.lazy.rememberLazyListState
