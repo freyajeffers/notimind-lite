a68b7231fd24bee203eb74d2100d257f5823a247
Freya Jeffers <fboniface@unr.edu>
2026-09-24 19:08:48 -0700

    chore(build): constrain transient deps to patched versions (jdom, commons-lang3, httpclient, guava)

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -28,6 +28,22 @@ dependencyResolutionManagement {
   }
 }
 
+// Keep any dependency that is introduced by a plugin or transitive graph at a
+// patched version. These rules are harmless when the module is absent and
+// prevent vulnerable versions from entering any project configuration.
+gradle.beforeProject {
+  configurations.configureEach {
+    resolutionStrategy.eachDependency {
+      when (requested.group to requested.name) {
+        "org.jdom" to "jdom2" -> useVersion("2.0.6.1")
+        "org.apache.commons" to "commons-lang3" -> useVersion("3.18.0")
+        "org.apache.httpcomponents" to "httpclient" -> useVersion("4.5.13")
+        "com.google.guava" to "guava" -> useVersion("32.1.3-android")
+      }
+    }
+  }
+}
+
 rootProject.name = "notimind lite"
 
 include(":app")
